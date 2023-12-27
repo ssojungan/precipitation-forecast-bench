d51edffa54255bcfc5ed3b73ad18e8b4ad85cf8e
ssojungan <114484363+ssojungan@users.noreply.github.com>
2023-12-27 17:09:12 +0900

    Create utils.py

---
+++ model/trajgru/utils.py
@@ -0,0 +1,45 @@
+from torch import nn
+from collections import OrderedDict
+
+def make_layers(block):
+    layers = []
+    for layer_name, v in block.items():
+        if 'pool' in layer_name:
+            layer = nn.MaxPool2d(kernel_size=v[0], stride=v[1], padding=v[2])
+            layers.append((layer_name, layer))
+        elif 'deconv' in layer_name:
+            transposeConv2d = nn.ConvTranspose2d(in_channels=v[0],
+                                                 out_channels=v[1],
+                                                 kernel_size=v[2],
+                                                 stride=v[3],
+                                                 padding=v[4])
+            layers.append((layer_name, transposeConv2d))
+            layers.append(('bn_' + layer_name, nn.BatchNorm2d(v[1])))
+            if 'relu' in layer_name:
+                layers.append(('relu_' + layer_name, nn.ReLU(inplace=True)))
+            elif 'leaky' in layer_name:
+                layers.append(('leaky_' + layer_name,
+                               nn.LeakyReLU(negative_slope=0.2, inplace=True)))
+            elif 'gelu' in layer_name:
+                layers.append(('gelu_' + layer_name,
+                               nn.GELU()))
+
+        elif 'conv' in layer_name:
+            conv2d = nn.Conv2d(in_channels=v[0],
+                               out_channels=v[1],
+                               kernel_size=v[2],
+                               stride=v[3],
+                               padding=v[4])
+            layers.append((layer_name, conv2d))
+            layers.append(('bn_' + layer_name, nn.BatchNorm2d(v[1])))
+            if 'relu' in layer_name:
+                layers.append(('relu_' + layer_name, nn.ReLU(inplace=True)))
+            elif 'leaky' in layer_name:
+                layers.append(('leaky_' + layer_name,
+                               nn.LeakyReLU(negative_slope=0.2, inplace=True)))
+            elif 'gelu' in layer_name:
+                layers.append(('gelu_' + layer_name,
+                               nn.GELU()))
+        else:
+            raise NotImplementedError
+    return nn.Sequential(OrderedDict(layers))
